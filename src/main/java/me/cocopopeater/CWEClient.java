26fbda918363fe2c70daa4b09f75ec46b6f492af
CocoPopEater <iamerror405@gmail.com>
2024-11-13 12:42:22 +0000

    Init

---
+++ src/main/java/me/cocopopeater/CWEClient.java
@@ -0,0 +1,10 @@
+package me.cocopopeater;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CWEClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
